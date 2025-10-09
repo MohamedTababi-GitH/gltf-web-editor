5547e70581d14fd25f47f3cc91182c80352ac5af
Abol3ninn <ahmed.aboelenein@outlook.com>
2025-10-09 12:42:11 +0200

    Added Context Menu for ModelListItem Made Model List Page responsive

--- Frontend/src/components/ListView/ListView.tsx
+++ Frontend/src/components/ListView/ListView.tsx
@@ -1,4 +1,4 @@
-import ModelListItem from "@/components/ModelListItem.tsx";
+import ModelListItem from "@/components/ListView/ModelListItem.tsx";
 
 function ListView() {
   const data = [
@@ -27,25 +27,49 @@ function ListView() {
       size: "50 MB",
       date: new Date(),
     },
+    {
+      name: "Model 6",
+      size: "50 MB",
+      date: new Date(),
+    },
+    {
+      name: "Model 7",
+      size: "50 MB",
+      date: new Date(),
+    },
+    {
+      name: "Model 8",
+      size: "50 MB",
+      date: new Date(),
+    },
+    {
+      name: "Model 9",
+      size: "50 MB",
+      date: new Date(),
+    },
   ];
 
   return (
-    <>
-      <div className={`m-6`}>
-        <div className={`flex justify-between px-2`}>
-          <h1 className={`text-2xl font-medium mb-3`}>Table of Models</h1>
-          <h1 className={`text-lg font-medium text-muted-foreground mb-3`}>
+    <div className={`grid justify-center w-full`}>
+      <div className={`m-4 md:m-8 lg:m-12 xl:m-16 max-w-screen `}>
+        <div
+          className={`flex justify-between px-2 font-medium text-sm md:text-lg lg:text-xl`}
+        >
+          <h1 className={`mb-3`}>Table of Models</h1>
+          <h1 className={`text-muted-foreground mb-3`}>
             {data.length} results found
           </h1>
         </div>
 
-        <div className={`grid grid-cols-4 gap-4`}>
+        <div
+          className={`grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 w-full`}
+        >
           {data.map((item, i) => (
             <ModelListItem key={i} item={item} />
           ))}
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
