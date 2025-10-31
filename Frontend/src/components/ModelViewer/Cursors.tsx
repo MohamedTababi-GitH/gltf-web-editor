cae80a77ec330fdbd9bd39bcfa8912de1caead0e
Manav <mdave0905@gmail.com>
2025-10-31 15:57:04 +0100

    Keyboard shortcuts and tooltips

--- Frontend/src/components/ModelViewer/Cursors.tsx
+++ Frontend/src/components/ModelViewer/Cursors.tsx
@@ -24,17 +24,21 @@ function Cursors({
   setSelectedTool: (tool: Cursor) => void;
   selectedTool: string;
 }) {
-  const tools: { name: Cursor; icon: ComponentType<LucideProps> }[] = [
-    { name: CursorEnum.Select, icon: MousePointer },
-    { name: CursorEnum.MultiSelect, icon: SquareStack },
-    { name: CursorEnum.Move, icon: Move },
-    { name: CursorEnum.Translate, icon: MoveHorizontal },
-    { name: CursorEnum.Scale, icon: Scaling },
-    { name: CursorEnum.Rotate, icon: RotateCw },
+  const tools: {
+    name: Cursor;
+    icon: ComponentType<LucideProps>;
+    shortcut: string;
+  }[] = [
+    { name: CursorEnum.Select, icon: MousePointer, shortcut: "S" },
+    { name: CursorEnum.MultiSelect, icon: SquareStack, shortcut: "X" },
+    { name: CursorEnum.Move, icon: Move, shortcut: "M" },
+    { name: CursorEnum.Translate, icon: MoveHorizontal, shortcut: "T" },
+    { name: CursorEnum.Scale, icon: Scaling, shortcut: "C" },
+    { name: CursorEnum.Rotate, icon: RotateCw, shortcut: "R" },
   ];
   return (
     <div className="absolute left-4 top-1/2 z-10 -translate-y-1/2  sm:mt-4">
-      <div className="flex flex-col items-center gap-1.5 lg:gap-2 rounded-md lg:rounded-xl border bg-popover/60 p-1.25 lg:p-2 text-popover-foreground shadow-lg backdrop-blur-xl">
+      <div className="flex flex-col items-center gap-1.5 lg:gap-2 rounded-md md:rounded-lg lg:rounded-xl border bg-popover/60 p-1.25 lg:p-2 text-popover-foreground shadow-lg backdrop-blur-xl">
         {tools.map((tool) => (
           <Tooltip key={tool.name}>
             <TooltipTrigger asChild={true}>
@@ -42,13 +46,15 @@ function Cursors({
                 onClick={() => setSelectedTool(tool.name)}
                 variant="default"
                 size="icon"
-                className={`rounded-sm lg:rounded-lg w-6 h-6 lg:w-12 lg:h-12 ${tool.name === selectedTool ? "bg-primary text-background" : "bg-popover text-foreground hover:bg-popover/90 hover:text-foreground"}`}
+                className={`rounded-sm md:rounded-md lg:rounded-lg w-6 h-6 md:w-9 md:h-9 lg:w-12 lg:h-12 ${tool.name === selectedTool ? "bg-primary text-background" : "bg-popover text-foreground hover:bg-popover/90 hover:text-foreground"}`}
               >
-                <tool.icon />
+                <tool.icon className="size-3 md:size-4 lg:size-5" />
               </Button>
             </TooltipTrigger>
             <TooltipContent side="right">
-              <p>{tool.name}</p>
+              <p>
+                {tool.name} ({tool.shortcut})
+              </p>
             </TooltipContent>
           </Tooltip>
         ))}
