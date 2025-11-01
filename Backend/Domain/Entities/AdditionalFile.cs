f9b3d420626c96c259065dc30df96f7e57ce818e
keks <kekschorstviy@gmail.com>
2025-11-01 19:54:18 +0100

    timestamp in add files

--- Backend/Domain/Entities/AdditionalFile.cs
+++ Backend/Domain/Entities/AdditionalFile.cs
@@ -5,5 +5,6 @@ public class AdditionalFile
     public required string Name { get; init; }
     public required Uri Url { get; init; }
     public long? SizeBytes { get; init; }
+    public DateTimeOffset? CreatedOn { get; init; }
     public string? ContentType { get; init; }
 }
