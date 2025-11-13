e7ec64a198409801af5aa241a9fae15b01598c7d
Henrik <thimhe01@thu.de>
2025-11-13 11:15:15 +0100

    Added Tests for Entities

---
+++ Backend/UnitTests/Entities/AdditionalFileTest.cs
@@ -0,0 +1,55 @@
+﻿using ECAD_Backend.Domain.Entities;
+
+namespace ECAD_Backend.UnitTests.Entities;
+
+[TestClass]
+public class AdditionalFileTest
+{
+    [TestMethod]
+    public void Constructor_AssignsProperties()
+    {
+        // Arrange
+        var name = "name";
+        var url = new Uri("http://model/name");
+        var sizeBytes = 512L;
+        var createdOn = new DateTimeOffset(2022, 11, 11, 22, 11, 11,  TimeSpan.Zero);
+        var contentType = "image/png";
+        
+        // Act
+        var additionalFile = new AdditionalFile
+        {
+            Name = name,
+            Url = url,
+            SizeBytes = sizeBytes,
+            CreatedOn = createdOn,
+            ContentType = contentType
+        };
+        
+        // Assert
+        Assert.AreEqual(name, additionalFile.Name);
+        Assert.AreEqual(url, additionalFile.Url);
+        Assert.AreEqual(sizeBytes, additionalFile.SizeBytes);
+        Assert.AreEqual(createdOn, additionalFile.CreatedOn);
+        Assert.AreEqual(contentType, additionalFile.ContentType);
+    }
+
+    [TestMethod]
+    public void Constructor_OptionalProperties_CanBeNull()
+    {
+        // Arrange
+        var name = "name";
+        var url = new Uri("http://model/name");
+        
+        // Act
+        var additionalFile = new AdditionalFile
+        {
+            Name = name,
+            Url = url,
+        };
+        
+        // Assert
+        Assert.IsNull(additionalFile.SizeBytes);
+        Assert.IsNull(additionalFile.CreatedOn);
+        Assert.IsNull(additionalFile.ContentType);
+    }
+}
